174bfc5edf4db0e7db4a39d2b344faff039825ae
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-25 22:31:46 +0200

    Some modification

--- mkdiruby.rb
+++ mkdiruby.rb
@@ -17,9 +17,8 @@ end
 
 ########### GEMFILE
 def create_gemfile(folder_name)
-  gemfile = File.open("./#{folder_name}/Gemfile", "w")
-  gems = ["source \"https://rubygems.org\"", "ruby '2.7.4', '~> 0.57.2'", "gem 'rubocop'", "gem 'rspec'", "gem 'pry'", "gem 'dotenv'"]
-  gemfile = File.open("./#{folder_name}/Gemfile", "a")
+  gemfile = File.open("./#{folder_name}/Gemfile", "w+")
+  gems = ["source \"https://rubygems.org\"", "ruby '2.7.4'", "gem 'rubocop'", "gem 'rspec'", "gem 'pry'", "gem 'dotenv'"]
   gems.each {|gem| gemfile.puts(gem)}
   gemfile.close
 end
@@ -49,15 +48,13 @@ end
 
 ########### .ENV
 def env(folder_name)
-  env = File.open("./#{folder_name}/.env", "w")
-  env.close
+  File.open("./#{folder_name}/.env", "w")
 end
 
 
 ########### .GITIGNORE
 def gitignore(folder_name)
-  gitignore = File.open("./#{folder_name}/.gitignore", "w")
-  gitignore = File.open("./#{folder_name}/.gitignore", "a")
+  gitignore = File.open("./#{folder_name}/.gitignore", "w+")
   gitignore.puts(".env")
   gitignore.close
 end
@@ -66,6 +63,16 @@ end
 ########### README
 def readme(folder_name)
   readme = File.open("./#{folder_name}/README.md", "w")
+  readme.puts("# NAME\n\n\n## 1. Download the source code\n```ruby\n$ git clone https://github.com/entertotheneworld/XXXXX\n$ cd XXXXX\n```\n\n\n## 2. Install Gem\n```ruby\n$ gem install bundler\n$ bundle install\n$ rspec --init\n```\nTo verify the installation you must have a Gemfile.lock file in the folder.\n⚠️ COMMON ERROR : Check that your version of ruby ​​matches the one saved in the gem file.\n\n\n## 3. Usage\n```ruby\n$ cd lib\n$ ruby app.rb\n```")
+  readme.close
+end
+
+
+########### APP.RB
+def app(folder_name)
+  app = File.open("./#{folder_name}/app.rb", "w+")
+  app.puts("require 'bundler'\nBundler.require\nrequire_relative 'lib/'")
+  app.close
 end
 
 
@@ -97,6 +104,9 @@ def perform
 
   # Readme    
   readme(folder_name)
+
+  # App.rb
+  app(folder_name)
 end
 
 perform
